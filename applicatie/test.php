4ded0ecd7e32ed0d064faea863b265340c74c68e
DaanWol2004 <dt.woltjer@student.han.nl>
2025-01-20 12:06:45 +0100

    db testing

---
+++ applicatie/test.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'db_connectie.php';
+
+// maak verbinding met de database (zie db_connection.php)
+$db = maakVerbinding();
+
+// haal alle componisten op en tel het aantal stukken
+$query = 'select max(name) as name from ProductType';
+
+$data = $db->query($query);
+
+while($rij = $data->fetch()) {
+    echo $rij['name'];
+}
